83c4440b5c919e5b4541179f2d4056f3f6ce08f4
Timur Polatov <polatov@MacBook-Pro-Timur.local>
2023-11-23 13:46:32 +0600

    изменено:      README.md 	изменено:      brain_games/scripts/__pycache__/brain_games.cpython-311.pyc 	изменено:      brain_games/scripts/brain_games.py 	изменено:      dist/hexlet_code-0.1.0-py3-none-any.whl 	изменено:      dist/hexlet_code-0.1.0.tar.gz

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-__package__ = 'brain_games'
 from brain_games.cli import welcome_user
-from brain_games.scripts.brain_even import brain_game
 
 
 def main():
     print("Welcome to the Brain Games!")
     user_name = welcome_user()
-    brain_game(user_name)
 
 
 if __name__ == '__main__':
