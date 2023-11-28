8cfa8044a24433a6bb49db5119d187c635ac8f14
polatov <timur@polatov.me>
2023-11-28 17:08:46 +0600

    updated

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -10,7 +10,9 @@ def play_brain_prime(name):
         number = random.randint(1, 99)
         print(f"Question: {number}")
         user_answer = prompt.string("Your answer: ")
-        if number % 2 != 0:
+        if number == 1:
+            correct_answer = 'yes'
+        elif number % 2 != 0:
             correct_answer = 'yes'
         else:
             correct_answer = 'no'
