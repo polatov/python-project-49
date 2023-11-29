cbe6dcc2e193682b237f8f78a542e59adaf262a4
polatov <timur@polatov.me>
2023-11-29 10:43:05 +0600

    updated

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-# from brain_games.cli import welcome_user
+from brain_games.cli import welcome_user
 
 
 def print_welcome_message():
     print("Welcome to the Brain Games!")
 
 
-def main() -> object:
+def main():
     print_welcome_message()
-    # user_name = welcome_user()
+    user_name = welcome_user()
 
 
 if __name__ == '__main__':
